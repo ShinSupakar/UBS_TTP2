b8740448a0202c05f137d88ced5c38a2ce66bdc3
Shinjini Supakar <128966299+ShinSupakar@users.noreply.github.com>
2024-05-17 08:05:38 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline, Container, Typography, Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import SignUpPage from './SignUpPage';
 import SignUpForm from './SignUpForm';
 
-const App = () => {
+// Define a theme with custom font
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Helvetica Neue, serif',
+  },
+});
+
+function App() {
   return (
-    <div>
-      <h1>User Sign-Up</h1>
-      <SignUpForm />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router> {/* Ensure that the App component is wrapped with the Router component */}
+        <Container maxWidth="sm">
+          <Box sx={{ textAlign: 'center', mt: 20 }}>
+          <Typography variant="h3" component="h1" gutterBottom>
+              User Sign-Up
+            </Typography>
+            <Routes>
+              <Route exact path="/" element={<SignUpForm />} />
+              <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+          </Box>
+        </Container>
+      </Router>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
+
 
