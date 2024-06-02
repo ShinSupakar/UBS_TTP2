01619717e15d208f2fa36245cf699c9d606af0e5
Shinjini Supakar <128966299+ShinSupakar@users.noreply.github.com>
2024-06-02 16:53:09 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline, Container, Typography, Box } from '@mui/material';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { CssBaseline, Box } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import SignUpPage from './SignUpPage';
 import SignUpForm from './SignUpForm';
+import NewScreen from './NewScreen';
+import QuizPage from './QuizPage';
+import CustomContentPage from './CustomContentPage';
+import ProfilePage from './ProfilePage'; 
+import ForumPage from './ForumPage'; 
+import InclusiveResourcesPage from './InclusiveResourcesPage'; // Import the new page
 
-// Define a theme with custom font
 const theme = createTheme({
   typography: {
     fontFamily: 'Helvetica Neue, serif',
@@ -16,23 +21,43 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router> {/* Ensure that the App component is wrapped with the Router component */}
-        <Container maxWidth="sm">
-          <Box sx={{ textAlign: 'center', mt: 20 }}>
-          <Typography variant="h3" component="h1" gutterBottom>
-              User Sign-Up
-            </Typography>
-            <Routes>
-              <Route exact path="/" element={<SignUpForm />} />
-              <Route path="/signup" element={<SignUpPage />} />
-            </Routes>
-          </Box>
-        </Container>
+      <Router>
+        <Routes>
+          <Route path="/newscreen" element={<NewScreenLayout />} />
+          <Route path="/quizpage" element={<QuizPage />} />
+          <Route path="/customcontent" element={<CustomContentPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/forum" element={<ForumPage />} />
+          <Route path="/inclusive-resources" element={<InclusiveResourcesPage />} /> {/* Add the new route */}
+          <Route path="*" element={<DefaultLayout />} />
+        </Routes>
       </Router>
     </ThemeProvider>
   );
 }
 
-export default App;
+function DefaultLayout() {
+  const location = useLocation();
 
+  return (
+    <Box sx={{ textAlign: 'center', mt: 2, width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: '#f5f5f5' }}>
+      <Box sx={{ width: '100%', maxWidth: 'md' }}>
+        <Routes>
+          <Route exact path="/" element={<SignUpForm />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
+}
+
+function NewScreenLayout() {
+  return (
+    <Box sx={{ width: '100%', height: '100vh', padding: 0 }}>
+      <NewScreen />
+    </Box>
+  );
+}
+
+export default App;
 
